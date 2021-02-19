4d9a7d32498221b6617506beeab6b0f8e18b0f4e
Iva Trifonova <ivatrifonova15@gmail.com>
2021-02-19 18:06:02 +0200

    home objects and home constructor

---
+++ Home.js
@@ -0,0 +1,13 @@
+class Home {
+    constructor(id, name, type, price, capacity,image,location, booked, pets) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+        this.price = price;
+        this.capacity = capacity;
+        this.image = image;
+        this.location = location;
+        this.booked = booked;
+        this.pets = pets;
+    }
+}
