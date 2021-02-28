9107292c83c9b7ae22447db00a44dff622ecdb97
Martin Hristov <martinhrist@abv.bg>
2021-02-28 13:40:09 +0200

    Created encapsulation in index.js and onlineExperiencesFunctions.js files and fixed a minor SPA router bug

--- Controller/index.js
+++ Controller/index.js
@@ -1,58 +1,69 @@
 "use strict";
-//SPA Router
-function Router() {
-  let url = location.hash.slice(1);
+(function () {
+  //SPA Router
+  // Initial DOM elements selectors
+  let homePage = getById("PlacesToStay");
+  let expPage = getById("Experiences");
+  let onlineExpPage = getById("Online-Experiences");
+  let myPageTitle = getById("page-title");
+  let objectsSection = getById(
+    "onlineExperience-experiences-section-container"
+  );
+  let hasLoaded = false;
+  function Router() {
+    let url = location.hash.slice(1);
 
-  switch (url) {
-    case "PlacesToStay": {
-      placesToStay();
-      break;
-    }
-    case "Experiences": {
-      experiences();
-      break;
-    }
-    case "Online-Experiences": {
-      onlineExperiences();
-      break;
-    }
-    default: {
-      defaultCase();
-      break;
+    switch (url) {
+      case "PlacesToStay": {
+        placesToStay();
+        break;
+      }
+      case "Experiences": {
+        experiences();
+        break;
+      }
+      case "Online-Experiences": {
+        onlineExperiences();
+        break;
+      }
+      default: {
+        defaultCase();
+        break;
+      }
     }
   }
-}
-function placesToStay() {
-  homePage.style.display = "flex";
-  expPage.style.display = "none";
-  onlineExpPage.style.display = "none";
-  myPageTitle.innerText =
-    "Vacation Rentals, Homes, Experiences & Places - Airbnb";
-}
-function experiences() {
-  homePage.style.display = "block";
-  expPage.style.display = "none";
-  onlineExpPage.style.display = "none";
-}
-function onlineExperiences() {
-  homePage.style.display = "none";
-  expPage.style.display = "none";
-  onlineExpPage.style.display = "block";
-  myPageTitle.innerText = "Online Experiences";
-  if (!hasLoaded) {
-    showOnlineExperiences(
-      myManager.onlineExperiences,
-      objectsSection,
-      startValue,
-      endValue
-    );
-    hasLoaded = true;
+  function placesToStay() {
+    homePage.style.display = "flex";
+    expPage.style.display = "none";
+    onlineExpPage.style.display = "none";
+    myPageTitle.innerText =
+      "Vacation Rentals, Homes, Experiences & Places - Airbnb";
   }
-}
-function defaultCase() {
-  homePage.style.display = "block";
-  expPage.style.display = "none";
-  onlineExpPage.style.display = "none";
-}
-window.addEventListener("hashchange", Router);
-window.addEventListener("DOMContentLoaded", Router);
+  function experiences() {
+    homePage.style.display = "block";
+    expPage.style.display = "none";
+    onlineExpPage.style.display = "none";
+  }
+  function onlineExperiences() {
+    homePage.style.display = "none";
+    expPage.style.display = "none";
+    onlineExpPage.style.display = "block";
+    myPageTitle.innerText = "Online Experiences";
+    if (!hasLoaded) {
+      showOnlineExperiences(
+        myManager.onlineExperiences,
+        objectsSection,
+        startValue,
+        endValue
+      );
+      hasLoaded = true;
+    }
+  }
+  function defaultCase() {
+    homePage.style.display = "block";
+    expPage.style.display = "none";
+    onlineExpPage.style.display = "none";
+  }
+  window.addEventListener("hashchange", Router);
+  window.addEventListener("DOMContentLoaded", Router);
+})();
--- Controller/onlineExperiencesFunctions.js
+++ Controller/onlineExperiencesFunctions.js
@@ -1,104 +1,105 @@
 //filling all elements into the manager
-myObjects.forEach(function (object) {
-  let onlineExperience = new OnlineExperiences(
-    object.picture,
-    object.rating,
-    object.title,
-    object.visitedTimes,
-    object.location,
-    object.price,
-    object.category,
-    object.id
+(function () {
+  myObjects.forEach(function (object) {
+    let onlineExperience = new OnlineExperiences(
+      object.picture,
+      object.rating,
+      object.title,
+      object.visitedTimes,
+      object.location,
+      object.price,
+      object.category,
+      object.id
+    );
+    myManager.addExperience(onlineExperience);
+  });
+
+  // Initial DOM elements selectors
+  let objectsSection = getById(
+    "onlineExperience-experiences-section-container"
   );
-  myManager.addExperience(onlineExperience);
-});
+  let BtnLoadMore = getById("btn-loadMore");
+  let filterSectionCategories = getById("filter-section-categories");
+  let startValue = 0;
+  let endValue = 12;
 
-// Initial DOM elements selectors
-let homePage = getById("PlacesToStay");
-let expPage = getById("Experiences");
-let onlineExpPage = getById("Online-Experiences");
-let OnlineExpLogo = getById("online-exp-logo");
-let locationExperiences = getById("location");
-let myPageTitle = getById("page-title");
-let objectsSection = getById("onlineExperience-experiences-section-container");
-let BtnLoadMore = getById("btn-loadMore");
-let filterSectionCategories = getById("filter-section-categories");
-let btnEntertainment = getById("btn-category-entertainment");
-let btnMagic = getById("btn-category-magic");
-let hasLoaded = false;
-let startValue = 0;
-let endValue = 12;
+  function showOnlineExperiences(Experiences, container, start, showObjects) {
+    container.innerHTML = "";
+    for (let i = start; i < showObjects; i++) {
+      let current = Experiences[i];
+      let experienceCard = createEl("div");
+      experienceCard.style.marginRight = "1vw";
+      experienceCard.style.marginBottom = "1vw";
+      experienceCard.style.cursor = "pointer";
+      experienceCard.class = "experience-card";
+      let img = createEl("img");
+      img.style.width = "14vw";
+      img.style.height = "18.5vw";
+      img.style.borderRadius = "1vw";
+      img.alt = "snimka";
+      img.src = current.picture;
+      let expTitle = createEl("p", current.title);
+      expTitle.style.width = "12vw";
+      let firstRowDiv = createEl("div");
+      firstRowDiv.style.display = "flex";
+      firstRowDiv.style.fontSize = "0.9vw";
+      firstRowDiv.style.marginBottom = "0.5vw";
+      let starIcon = createEl("i");
+      starIcon.className = "far fa-star";
+      starIcon.style.color = "red";
+      let expRating = createEl("p", current.rating);
+      expRating.style.marginRight = "0.3vw";
+      let expVisited = createEl("p", "(" + current.visitedTimes + ") . ");
+      expVisited.style.opacity = "0.6";
+      let expLocation = createEl("p", current.location);
+      expLocation.style.opacity = "0.6";
+      expLocation.style.marginLeft = "0.3vw";
 
-function showOnlineExperiences(Experiences, container, start, showObjects) {
-  container.innerHTML = "";
-  for (let i = start; i < showObjects; i++) {
-    let current = Experiences[i];
-    let experienceCard = createEl("div");
-    experienceCard.style.marginRight = "1vw";
-    experienceCard.style.marginBottom = "1vw";
-    experienceCard.style.cursor = "pointer";
-    experienceCard.class = "experience-card";
-    let img = createEl("img");
-    img.style.width = "14vw";
-    img.style.height = "18.5vw";
-    img.style.borderRadius = "1vw";
-    img.alt = "snimka";
-    img.src = current.picture;
-    let expTitle = createEl("p", current.title);
-    expTitle.style.width = "12vw";
-    let firstRowDiv = createEl("div");
-    firstRowDiv.style.display = "flex";
-    firstRowDiv.style.fontSize = "0.9vw";
-    firstRowDiv.style.marginBottom = "0.5vw";
-    let starIcon = createEl("i");
-    starIcon.className = "far fa-star";
-    starIcon.style.color = "red";
-    let expRating = createEl("p", current.rating);
-    expRating.style.marginRight = "0.3vw";
-    let expVisited = createEl("p", "(" + current.visitedTimes + ") . ");
-    expVisited.style.opacity = "0.6";
-    let expLocation = createEl("p", current.location);
-    expLocation.style.opacity = "0.6";
-    expLocation.style.marginLeft = "0.3vw";
-
-    let expPrice = createEl("p", "From " + current.price + "лв./ person");
-    expPrice.style.fontWeight = "bold";
-    container.append(experienceCard);
-    experienceCard.append(img);
-    experienceCard.append(firstRowDiv);
-    firstRowDiv.append(starIcon);
-    firstRowDiv.append(expRating);
-    firstRowDiv.append(expVisited);
-    firstRowDiv.append(expLocation);
-    experienceCard.append(expTitle);
-    experienceCard.append(expPrice);
+      let expPrice = createEl("p", "From " + current.price + "лв./ person");
+      expPrice.style.fontWeight = "bold";
+      container.append(experienceCard);
+      experienceCard.append(img);
+      experienceCard.append(firstRowDiv);
+      firstRowDiv.append(starIcon);
+      firstRowDiv.append(expRating);
+      firstRowDiv.append(expVisited);
+      firstRowDiv.append(expLocation);
+      experienceCard.append(expTitle);
+      experienceCard.append(expPrice);
+    }
+    endValue += 6;
+    if (endValue > Experiences.length) {
+      BtnLoadMore.style.display = "none";
+    }
   }
-  endValue += 6;
-  if (endValue > Experiences.length) {
-    BtnLoadMore.style.display = "none";
-  }
-}
-
-BtnLoadMore.addEventListener("click", () => {
   showOnlineExperiences(
     myManager.onlineExperiences,
     objectsSection,
     startValue,
     endValue
   );
-});
-function addFilterButtons(filterSectionCategs) {
-  for (let i = 0; i < filterSectionCategs.length; i++) {
-    let current = filterSectionCategs[i];
-    current.addEventListener("click", () => {
-      myManager.filterBy("category", current.innerText);
-      showOnlineExperiences(
-        myManager.filtered,
-        objectsSection,
-        startValue,
-        myManager.filtered.length
-      );
-    });
+
+  BtnLoadMore.addEventListener("click", () => {
+    showOnlineExperiences(
+      myManager.onlineExperiences,
+      objectsSection,
+      startValue,
+      endValue
+    );
+  });
+  function addFilterButtons(filterSectionCategs) {
+    for (let i = 0; i < filterSectionCategs.length; i++) {
+      let current = filterSectionCategs[i];
+      current.addEventListener("click", () => {
+        myManager.filterBy("category", current.innerText);
+        showOnlineExperiences(
+          myManager.filtered,
+          objectsSection,
+          startValue,
+          myManager.filtered.length
+        );
+      });
+    }
   }
-}
-addFilterButtons(filterSectionCategories.children);
+  addFilterButtons(filterSectionCategories.children);
+})();
